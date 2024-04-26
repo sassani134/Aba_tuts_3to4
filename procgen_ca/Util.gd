6499504205095197dc9fac3cd0590eb151b048e4
sassani134 <sassani134@gmail.com>
2024-04-26 10:33:54 +0200

    WIP Util.gd
    - get_type or typeof choices[rand_index] to make a properly typed the func
    - reread the tuts https://abitawake.com/news/articles/procedural-generation-with-godot-creating-caves-with-cellular-automata

--- procgen_ca/Util.gd
+++ procgen_ca/Util.gd
@@ -1,9 +1,9 @@
-tool
+@tool
 extends Node
 
 
 # the percent chance something happens
-func chance(num):
+func chance(num : int) -> bool:
 	randomize()
 
 	if randi() % 100 <= num:  return true
@@ -16,3 +16,5 @@ func choose(choices):
 
 	var rand_index = randi() % choices.size()
 	return choices[rand_index]
+# get_type or typeof choices[rand_index] to make a properly typed the func
+# reread the tuts https://abitawake.com/news/articles/procedural-generation-with-godot-creating-caves-with-cellular-automata
